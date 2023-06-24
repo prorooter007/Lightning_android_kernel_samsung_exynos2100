b4911962894be3b26ec8b455fb3662505ad0579e
Shreyash Wasnik <shreyashwasnik112@gmail.com>
2023-06-24 11:24:49 +0530

    tools: Import OEM Changes
    Signed-off-by: Shreyash Wasnik <shreyashwasnik112@gmail.com>

--- tools/testing/kunit/kunit_config.py
+++ tools/testing/kunit/kunit_config.py
@@ -1,24 +1,20 @@
 # SPDX-License-Identifier: GPL-2.0
-#
-# Builds a .config from a kunitconfig.
-#
-# Copyright (C) 2019, Google LLC.
-# Author: Felix Guo <felixguoxiuping@gmail.com>
-# Author: Brendan Higgins <brendanhiggins@google.com>
 
 import collections
 import re
 
-CONFIG_IS_NOT_SET_PATTERN = r'^# CONFIG_\w+ is not set$'
-CONFIG_PATTERN = r'^CONFIG_\w+=(\S+|".*")$'
+CONFIG_IS_NOT_SET_PATTERN = r'^# CONFIG_(\w+) is not set$'
+CONFIG_PATTERN = r'^CONFIG_(\w+)=(\S+|".*")$'
 
-KconfigEntryBase = collections.namedtuple('KconfigEntry', ['raw_entry'])
-
+KconfigEntryBase = collections.namedtuple('KconfigEntry', ['name', 'value'])
 
 class KconfigEntry(KconfigEntryBase):
 
 	def __str__(self) -> str:
-		return self.raw_entry
+		if self.value == 'n':
+			return r'# CONFIG_%s is not set' % (self.name)
+		else:
+			return r'CONFIG_%s=%s' % (self.name, self.value)
 
 
 class KconfigParseError(Exception):
@@ -37,8 +33,21 @@ class Kconfig(object):
 	def add_entry(self, entry: KconfigEntry) -> None:
 		self._entries.append(entry)
 
-	def is_subset_of(self, other: 'Kconfig') -> bool:
-		return self.entries().issubset(other.entries())
+	def remove_entry(self, entry: KconfigEntry) -> None:
+		self._entries.remove(entry)
+
+	def is_subset_of(self, other: "Kconfig") -> bool:
+		for a in self.entries():
+			found = False
+			for b in other.entries():
+				if a.name != b.name:
+					continue
+				if a.value != b.value:
+					return False
+				found = True
+			if a.value != 'n' and found == False:
+				return False
+		return True
 
 	def write_to_file(self, path: str) -> None:
 		with open(path, 'w') as f:
@@ -54,9 +63,20 @@ class Kconfig(object):
 			line = line.strip()
 			if not line:
 				continue
-			elif config_matcher.match(line) or is_not_set_matcher.match(line):
-				self._entries.append(KconfigEntry(line))
-			elif line[0] == '#':
+
+			match = config_matcher.match(line)
+			if match:
+				entry = KconfigEntry(match.group(1), match.group(2))
+				self.add_entry(entry)
+				continue
+
+			empty_match = is_not_set_matcher.match(line)
+			if empty_match:
+				entry = KconfigEntry(empty_match.group(1), 'n')
+				self.add_entry(entry)
+				continue
+
+			if line[0] == '#':
 				continue
 			else:
 				raise KconfigParseError('Failed to parse: ' + line)
@@ -64,3 +84,10 @@ class Kconfig(object):
 	def read_from_file(self, path: str) -> None:
 		with open(path, 'r') as f:
 			self.parse_from_string(f.read())
+
+class KunitConfigProvider(object):
+
+  def get_kconfig(self, conf='kunitconfigs/kunitconfig') -> Kconfig:
+    kconfig = Kconfig()
+    kconfig.read_from_file(conf)
+    return kconfig
